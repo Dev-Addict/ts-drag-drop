49cc236e9966eedb527c1c1ddad5a83f33cec177
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-01 23:54:37 +0430

    getting inputs

--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
@@ -10,8 +13,45 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
 
         this.element = <HTMLFormElement>importedNode.firstElementChild;
+        this.element.id = 'user-input';
 
+        this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title');
+        this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description');
+        this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people');
+
+        this.configure();
         this.attach();
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    private getUserInput(): [string, string, number] | void {
+        const title = this.titleInputElement.value.trim();
+        const description = this.descriptionInputElement.value.trim();
+        const people = +this.peopleInputElement.value.trim();
+
+        if (!title || !description) {
+            alert('Invalid inputs.');
+            return;
+        }
+
+        this.clearInputs.bind(this)();
+
+        return [title, description, people];
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+
+        const userInput = this.getUserInput.bind(this)();
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 
     private attach() {
